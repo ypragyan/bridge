2781ef4b6dc9fdc692887a6788af229b57cfb7e0
Pragyan Yadav <72915754+ypragyan@users.noreply.github.com>
2025-02-02 10:41:02 -0500

    Update document.js

--- pages/document.js
+++ pages/document.js
@@ -1,24 +1,24 @@
-// pages/_document.js
-// import { Html, Head, Main, NextScript } from 'next/document'
+// // pages/_document.js
+// // import { Html, Head, Main, NextScript } from 'next/document'
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head>
-        {/* Example: Loading Open Sans from Google Fonts */}
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-        />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600&display=swap"
-          rel="stylesheet"
-        />
-      </Head>
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
-}
+// export default function Document() {
+//   return (
+//     <Html lang="en">
+//       <Head>
+//         {/* Example: Loading Open Sans from Google Fonts */}
+//         <link
+//           rel="preconnect"
+//           href="https://fonts.gstatic.com"
+//         />
+//         <link
+//           href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600&display=swap"
+//           rel="stylesheet"
+//         />
+//       </Head>
+//       <body>
+//         <Main />
+//         <NextScript />
+//       </body>
+//     </Html>
+//   )
+// }
