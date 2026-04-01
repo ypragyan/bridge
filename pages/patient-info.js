5b457d025c5f1b10d35b205455a4f649356f31f9
Pragyan Yadav <72915754+ypragyan@users.noreply.github.com>
2026-03-31 23:37:48 -0400

    Update import path for downloadpdf module

--- pages/patient-info.js
+++ pages/patient-info.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import Link from 'next/link';
-import downloadpdf from '../pages/download';
+import downloadpdf from '../lib/download';
 
 // pages/patient-info.js
 
